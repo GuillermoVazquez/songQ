cf4e8209d678461529373567d694f5205a95b7d5
Guillermo Vazquez <wvvazque@gmail.com>
2018-05-06 13:11:21 -0400

    omg it works. PUT request complete to spotify

--- songQ/php/play.php
+++ songQ/php/play.php
@@ -7,12 +7,14 @@ $headers = [
     'Content-Type: application/json',
     'Authorization: Bearer BQCva9OOcSmJHYuE-dINjkQeRQi8XxIJASk05lcnilUNGraONK2c3J5jRXSzc841JZFeAwgbBOm-dLio_RIFjlFw1TGgLJ9-Rx1herSW2wUkrJW3UHp16vjUf0XewmqOvKyLaMpPzBueofF4zLvNMLo8udPXqYqsji4ivHE'
 ];
+$json_data->uris = ["spotify:track:7iYlacahYFEKx8cpRNC2Tx"];
+$json_data = json_encode($json_data);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
+curl_setopt($ch, CURLOPT_POSTFIELDS,$json_data);
 $response = curl_exec($ch);
 
 //echo $response;
